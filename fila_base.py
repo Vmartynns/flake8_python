68d19ad887b335a9adcc5be3e0aca28b5d2bb388
Vmartynns <vm_martyns@hotmail.com>
2022-08-07 23:41:11 -0300

    Criando fila base

---
+++ fila_base.py
@@ -0,0 +1,11 @@
+class FilaBase:
+    codigo: int = 0
+    fila: list = []
+    clientes_atendidos: list = []
+    senha_atual: str = ""
+
+    def reseta_fila(self) -> None:
+        if self.codigo >= 100:
+            self.codigo = 0
+        else:
+            self.codigo += 1
--- fila_normal.py
+++ fila_normal.py
@@ -1,26 +1,16 @@
-class filanormal:
-    codigo:int = 0
-    fila = []
-    clientesatendidos = []
-    senhatual:str = ""
-    
-    def gerasenhatual(self)->None:
+from fila_base import FilaBase
+
+
+class FilaNormal(FilaBase):
+    def gerasenhatual(self) -> None:
         self.senhatual = "NM{}".format(self.codigo)
-        
-    def resetafila(self)->None:
-        if self.codigo >= 100:
-            self.codigo=0
-        else:
-            self.codigo+=1
-            
-    def atualizafila(self)->None:
-        self.resetafila()
+
+    def atualizafila(self) -> None:
+        self.reseta_fila()
         self.gerasenhatual()
         self.fila.append(self.senhatual)
-        
-    def chamacliente(self,caixa:int)->str:
-        cliente_atual:str = self.fila.pop(0)
-        self.clientesatendidos.append(cliente_atual)
-        return(f'Cliente atual: {cliente_atual}, dirija-se ao caixa: {caixa}')
-        
-    
+
+    def chamacliente(self, caixa: int) -> str:
+        cliente_atual: str = self.fila.pop(0)
+        self.clientes_atendidos.append(cliente_atual)
+        return (f'Cliente atual: {cliente_atual}, dirija-se ao caixa: {caixa}')
